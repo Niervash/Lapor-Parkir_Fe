a4153de1f88d7d333aeb687cc94c3a1cbaabd10d
Julian Witjaksono <151512615+Niervash@users.noreply.github.com>
2024-11-04 02:24:49 +0800

    update

--- src/component/pages/Pelapor/MenuPelaporanPage/ParkirLiar/ParkirLiarpage.jsx
+++ src/component/pages/Pelapor/MenuPelaporanPage/ParkirLiar/ParkirLiarpage.jsx
@@ -1,6 +1,6 @@
 // src/components/pages/Pelapor/MenuPelaporanPage/ParkirLiar/ParkirLiarpage.js
 import React from "react";
-import PelaporLayout from "../../../../Layout/pelaporLayout";
+import PelaporLayout from "../../../../Layout/PelaporLayout";
 import { ParkirLiar } from "../../../../Section/DashboardPelapor/Pelapor/parkirliar/ParkirLiar";
 
 export const ParkirLiarpage = () => {
