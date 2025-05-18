e6663ef71360ef868131b081f362b4dd4afe85db
Julian Witjaksono <151512615+Niervash@users.noreply.github.com>
2025-05-18 22:58:51 +0800

    Update 5/18/2025 Checkpoint FIX_All (menyesuaikan route commoncardused)

--- src/component/Section/DashboardPelapor/Home/HomeDashboard.jsx
+++ src/component/Section/DashboardPelapor/Home/HomeDashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { CommonCardused } from "../../../Fragment/CardCommon/CommonCardused";
+// import { CommonCardused } from "../../../Fragment/CardCommon/CommonCardused";
+import { CommonCardused } from "../../../Fragment/CardCommon/Commoncardused";
 import BerandaMaps from "../../../Fragment/maps/BerandaMaps";
 
 export const HomeDashboard = () => {
